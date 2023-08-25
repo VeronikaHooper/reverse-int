e1c4ae1c3b68d1129baa0af7a7a5a7d244d1dc09
VeronikaHooper <veronika20112000@gmail.com>
2023-08-25 15:03:15 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    const str = String(n);
+    const reversedString = str.split('').reverse().join('');
+
+    return parseInt(reversedString);
 }
